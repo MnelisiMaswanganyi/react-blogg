e3d0be7d887e9794d983dbc99d856af3e082f8d4
Jason <murangira99@gmail.com>
2024-03-06 23:17:53 +0200

    const express = require("express"); const router = express.Router(); const { Users } = require("../models"); const bcrypt = require("bcrypt");
    router.post("/", async (req, res) => {
      const { username, password } = req.body;
      bcrypt.hash(password, 10).then((hash) => {
        Users.create({
          username: username,
          password: hash,
        });
        res.json("SUCCESS");
      });
    });
    
    router.post("/login", async (req, res) => {
      const { username, password } = req.body;
    
      const user = await Users.findOne({ where: { username: username } });
    
      if (!user) res.json({ error: "User Doesn't Exist" });
    
      bcrypt.compare(password, user.password).then((match) => {
        if (!match) res.json({ error: "Wrong Username And Password Combination" });
    
        res.json("YOU LOGGED IN!!!");
      });
    });
    
    module.exports = router;

--- server/models/User.js
+++ server/models/User.js
@@ -1,19 +1,31 @@
-module.exports = (sequelize, DataTypes) => {
-    const Users = sequelize.define("Users", {
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+const express = require("express");
+const router = express.Router();
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
     });
-  
-    // Users.associate = (models) => {
-    //   Users.hasMany(models.Posts, {
-    //     onDelete: "cascade",
-    //   });
-    // };
-    return Users;
-  };
+    res.json("SUCCESS");
+  });
+});
+
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ where: { username: username } });
+
+  if (!user) res.json({ error: "User Doesn't Exist" });
+
+  bcrypt.compare(password, user.password).then((match) => {
+    if (!match) res.json({ error: "Wrong Username And Password Combination" });
+
+    res.json("YOU LOGGED IN!!!");
+  });
+});
+
+module.exports = router;
