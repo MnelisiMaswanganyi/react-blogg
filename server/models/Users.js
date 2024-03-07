d663d87277c4f260490d352bc9d8e1751bcedeac
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-07 18:35:48 +0200

    Remove User model and related routes. (generated by blackbox.ai )

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Users } = require("../models");
+const { Users } = require(".");
 const bcrypt = require("bcrypt");
 
 router.post("/", async (req, res) => {
