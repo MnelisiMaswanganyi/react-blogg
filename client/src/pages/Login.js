9795224d7fc40af01e326b4a8ae41a3ea96b4be2
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-07 16:57:54 +0200

    Add useHistory hook and push to root route on successful login. (generated by blackbox.ai )

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 function Login() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  let history = useHistory();
+
   const login = () => {
     const data = { username: username, password: password };
     axios.post("http://localhost:3001/auth/login", data).then((response) => {
       if (response.data.error) {
-       alert(response.data.error);
+        alert(response.data.error);
       } else {
-      sessionStorage.setItem("accessToken" response.data)
+        sessionStorage.setItem("accessToken", response.data);
+        history.push("/");
       }
     });
   };
