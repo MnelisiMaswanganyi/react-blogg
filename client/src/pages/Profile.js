b60676f331a125036503bba8b1bc375b87186be4
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-07 21:15:21 +0200

    Refactor code to use functional components and `useEffect` hook. (generated by blackbox.ai )

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,134 +1,54 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
-import { AuthContext } from "../helpers/AuthContext";
 
-function Post() {
+function Profile() {
   let { id } = useParams();
-  const [postObject, setPostObject] = useState({});
-  const [comments, setComments] = useState([]);
-  const [newComment, setNewComment] = useState("");
-  const { authState } = useContext(AuthContext);
-
   let history = useHistory();
+  const [username, setUsername] = useState("");
+  const [listOfPosts, setListOfPosts] = useState([]);
 
   useEffect(() => {
-    axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) => {
-      setPostObject(response.data);
+    axios.get(`http://localhost:3001/auth/basicinfo/${id}`).then((response) => {
+      setUsername(response.data.username);
     });
 
-    axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
-      setComments(response.data);
+    axios.get(`http://localhost:3001/posts/byuserId/${id}`).then((response) => {
+      setListOfPosts(response.data);
     });
   }, []);
 
-  const addComment = () => {
-    axios
-      .post(
-        "http://localhost:3001/comments",
-        {
-          commentBody: newComment,
-          PostId: id,
-        },
-        {
-          headers: {
-            accessToken: localStorage.getItem("accessToken"),
-          },
-        }
-      )
-      .then((response) => {
-        if (response.data.error) {
-          console.log(response.data.error);
-        } else {
-          const commentToAdd = {
-            commentBody: newComment,
-            username: response.data.username,
-          };
-          setComments([...comments, commentToAdd]);
-          setNewComment("");
-        }
-      });
-  };
-
-  const deleteComment = (id) => {
-    axios
-      .delete(`http://localhost:3001/comments/${id}`, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
-      })
-      .then(() => {
-        setComments(
-          comments.filter((val) => {
-            return val.id !== id;
-          })
-        );
-      });
-  };
-
-  const deletePost = (id) => {
-    axios
-      .delete(`http://localhost:3001/posts/${id}`, {
-        headers: { accessToken: localStorage.getItem("accessToken") },
-      })
-      .then(() => {
-        history.push("/");
-      });
-  };
   return (
-    <div className="postPage">
-      <div className="leftSide">
-        <div className="post" id="individual">
-          <div className="title"> {postObject.title} </div>
-          <div className="body">{postObject.postText}</div>
-          <div className="footer">
-            {postObject.username}
-            {authState.username === postObject.username && (
-              <button
+    <div className="profilePageContainer">
+      <div className="basicInfo">
+        {" "}
+        <h1> Username: {username} </h1>
+      </div>
+      <div className="listOfPosts">
+        {listOfPosts.map((value, key) => {
+          return (
+            <div key={key} className="post">
+              <div className="title"> {value.title} </div>
+              <div
+                className="body"
                 onClick={() => {
-                  deletePost(postObject.id);
+                  history.push(`/post/${value.id}`);
                 }}
               >
-                {" "}
-                Delete Post
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-      <div className="rightSide">
-        <div className="addCommentContainer">
-          <input
-            type="text"
-            placeholder="Comment..."
-            autoComplete="off"
-            value={newComment}
-            onChange={(event) => {
-              setNewComment(event.target.value);
-            }}
-          />
-          <button onClick={addComment}> Add Comment</button>
-        </div>
-        <div className="listOfComments">
-          {comments.map((comment, key) => {
-            return (
-              <div key={key} className="comment">
-                {comment.commentBody}
-                <label> Username: {comment.username}</label>
-                {authState.username === comment.username && (
-                  <button
-                    onClick={() => {
-                      deleteComment(comment.id);
-                    }}
-                  >
-                    X
-                  </button>
-                )}
+                {value.postText}
               </div>
-            );
-          })}
-        </div>
+              <div className="footer">
+                <div className="username">{value.username}</div>
+                <div className="buttons">
+                  <label> {value.Likes.length}</label>
+                </div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
 }
 
-export default Post;
+export default Profile;
