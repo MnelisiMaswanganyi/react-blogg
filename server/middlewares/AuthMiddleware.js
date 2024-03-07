c7a7b9414b21e72e27394eeb75c5b07f942aacba
Mnelisi Maswanganyi <142810215+MnelisiMaswanganyi@users.noreply.github.com>
2024-03-07 17:37:47 +0200

    Update authentication middleware and comments route
    - Update `AuthMiddleware` to use `req.user` instead of `accessToken` for authentication.
    - Update `Comments` route to include `username` in the comment object. (generated by blackbox.ai )

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -7,7 +7,7 @@ const validateToken = (req, res, next) => {
 
   try {
     const validToken = verify(accessToken, "importantsecret");
-
+req.user = "pedro"
     if (validToken) {
       return next();
     }
